9cd217b9999014608776af80e8ee650c7241a7aa
John Structurez <black.structure@yahoo.com>
2012-07-14 13:35:35 +0400

    -

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -7,19 +7,21 @@ module BoardHelper
     end
   end
   
-  def board_form_url
-    if(!defined? @thrd)
-      "/#{@board.name}/new"
-    else
-      "/#{@board.name}/#{@thrd.number}/new"
-    end
-  end
-  
   def board_post_url(post)
     board_thread_url(thrd: post.thrd) + '#' + post.number.to_s
   end
   
   def board_post_url_i(post)
     board_thread_url(thrd: post.thrd) + '#i' + post.number.to_s
+  end
+
+  def board_form_url(opts={})
+    board = (opts.include? :board) ? opts[:board] : @board
+
+    if(!defined? @thrd)
+      "/boards/#{board.name}/new"
+    else
+      "/boards/#{board.name}/#{@thrd.number}/new"
+    end
   end
 end
