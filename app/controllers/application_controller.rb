a01e371221b298d16674b4fea567b070eac232ff
John Structurez <black.structure@yahoo.com>
2012-07-14 13:35:08 +0400

    added shared helpers for board urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  #private
-  #def current_user
-  #  session[:current_user]
-  #end
+  helper_method :board_index_url, :board_thread_url # some shared helpers
+  
+  protected
+  
+  def board_index_url(opts={})
+    board = (opts.include? :board) ? opts[:board] : @board
+
+    if(opts.include? :page)
+      "/boards/#{board.name}/#{opts[:page]}"
+    else
+      "/boards/#{board.name}"
+    end
+  end
+  
+  def board_thread_url(opts={})
+    board = (opts.include? :board) ? opts[:board] : @board
+
+    if(opts.include? :thrd)
+      "/boards/#{board.name}/#{opts[:thrd].number}"
+    elsif(defined? @thrd)
+      "/boards/#{board.name}/#{@thrd.number}"
+    else
+      # TODO: raise exception
+      "/boards/#{board.name}"
+    end
+  end
+
 end
