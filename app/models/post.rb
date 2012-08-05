44a0372cd6667ee1df95cc8044d502037273ad9d
John Structurez <black.structure@yahoo.com>
2012-08-06 02:08:30 +0400

    added fileinfo fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,17 +10,31 @@ class Post
   field :body, type: String
   field :time, type: DateTime
   field :sage, type: Boolean, default: false
-
   mount_uploader :file, PostImageUploader
+  field :image_width, type: Integer
+  field :image_height, type: Integer
+  field :file_size, type: Integer
   
   index [ [:'board.name', Mongo::ASCENDING], [:number, Mongo::ASCENDING] ], unique: true
   
-  before_create :update_post_time
-  before_save :update_post_time
+  before_create :update_time, :update_fileinfo
+  before_save :update_time, :update_fileinfo
   
-  protected
-  def update_post_time
-    self.time= Time.now
+  def image
+    @image ||= MiniMagick::Image.open(file.path)
   end
   
+  def update_time
+    self.time= Time.now
+  end
+
+  def update_fileinfo
+    if self.file?
+      self.file_size ||= file.size
+      if !self.image_width || self.image_height
+        self.image_width ||= image['width']
+        self.image_height ||= image['height']
+      end
+    end
+  end
 end
