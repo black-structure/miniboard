0fef217a0f79dcb274b55b1fcfecd9ba4f293121
John Structurez <black.structure@yahoo.com>
2012-12-28 02:06:12 +0400

    -

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    user ||= User.new(roles: [ :guest ])
     
     if user.has_role? :admin
       can :manage, :all
