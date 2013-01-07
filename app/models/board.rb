6212ff616c683ec47620895a8ce64924821f5a53
John Structurez <black.structure@yahoo.com>
2013-01-07 16:18:40 +0400

    added replies_on_index field, added validators

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,12 +8,20 @@ class Board
   field :postername_default, type: String
   field :counter, type: Integer, default: 0
   field :perpage, type: Integer, default: 15
+  field :replies_on_index, type: Integer, default: 5
   field :maxthrds, type: Integer, default: 60
   field :maxfilesize, type: Integer, default: 1024*1024
   
   index({ name: 1 }, { name: 'index_board_name', unique: true })
   
   validates_presence_of :name
+
+  validate do |board|
+    errors.add(:perpage, 'This field allows only positive values') unless board.perpage >= 1
+    errors.add(:replies_on_index, 'This field allows only positive values') unless board.replies_on_index >= 1
+    errors.add(:maxthrds, 'This field allows only positive values') unless board.maxthrds >= 1
+    errors.add(:maxfilesize, 'This field allows only positive values') unless board.maxfilesize >= 1
+  end
   
   def inc_number
     inc(:counter, 1)
