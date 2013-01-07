9c621194f498f87a4a38dd8649c586195545e817
John Structurez <black.structure@yahoo.com>
2013-01-07 17:01:22 +0400

    added counters

--- app/models/thrd.rb
+++ app/models/thrd.rb
@@ -6,15 +6,20 @@ class Thrd
   
   field :number, type: Integer
   field :last_time, type: DateTime
+  field :count, type: Integer
+  field :count_files, type: Integer
 
   index({ board: 1, number: 1 }, { unique: true })
   
-  def count
-    posts.count
-  end
-  
   def bump!(post)
     update_attribute(:last_time, post.created_at)
+  end
+
+  def update_counters!(post)
+    if post
+      inc(:count, 1)
+      inc(:count_files, 1) if post.file?
+    end
   end
   
   store_in collection: 'threads'
